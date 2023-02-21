166615aa7bee99ffc790c216d782800690c13df1
Evgenii <eaturkin@yandex.ru>
2023-02-21 13:40:28 +0500

    MinNeap Sorting has been done

---
+++ Seminar4/HeapSorting.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+
+public class HeapSorting {
+    public static void main(String[] args){
+        int size = 10;
+        int[] array = fillingArray(size);
+        printArray(array);
+        System.out.println("After HeapSorting");
+        getBinaryTree(array);
+        printArray(array);
+        heapSort(array);
+        printArray(array);
+    }
+
+    public static int[] fillingArray(int size) {
+        Random rand = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(0, 100);
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int item : array) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+
+    public static void getBinaryTree(int[] array){
+        for (int i = array.length / 2 -1; i >= 0; i--) {
+            heapify(array, array.length, i);
+        }
+    }
+    public static void heapify(int[] array, int arrayLength, int index){
+        int n = arrayLength;
+        int largest = index;
+        int leftLeaf = index * 2 + 1;
+        int rightLeaf = index * 2 + 2;
+
+        if (leftLeaf < n && array[leftLeaf] > array[largest]){
+            largest = leftLeaf;
+        }
+
+        if (rightLeaf < n && array[rightLeaf] > array[largest]){
+            largest = rightLeaf;
+        }
+
+        if(largest != index) {
+            swap(array, index, largest);
+            heapify(array, n, largest);
+        }
+    }
+
+    public static void heapSort(int[] array){
+        for (int i = array.length-1; i >= 0 ; i--) {
+            swap(array, i, 0);
+            heapify(array, i, 0);
+        }
+    }
+
+    public static void swap(int[] array, int index1, int index2){
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+}
