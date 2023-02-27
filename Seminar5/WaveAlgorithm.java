e9bc20b9e17007a7c6a1c2e9db111fd6dc4c8503
Evgenii <eaturkin@yandex.ru>
2023-02-27 18:20:07 +0500

    illing of pitch has been done

--- Seminar5/WaveAlgorithm.java
+++ Seminar5/WaveAlgorithm.java
@@ -1,20 +1,22 @@
-import java.lang.Math;
+import java.util.ArrayList;
 
 public class WaveAlgorithm {
     public static void main(String[] args) {
-        int[][] pitch = getPitch();
-        int[][] wayPitch = getPitch();
+        String[][] pitch = getPitch();
+        String[][] wayPitch = getPitch();
 
-        int ax = 1;
-        int ay = 5;
+        int ax = 3;
+        int ay = 3;
         int bx = 0;
         int by = 0;
-        int counter = pitch[ay][ax];
-        fillingFreePitches(pitch, ax, ay, counter);
+        Integer mark = 1;
+        ArrayList<int[]> list = new ArrayList<>();
+        firstStep(pitch, ax, ay, list);
+        startFillingPitches(pitch, list, mark);
         printArray(pitch);
     }
 
-    static void printArray(int[][] array) {
+    static void printArray(String[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + "\t");
@@ -24,76 +26,79 @@ public class WaveAlgorithm {
         System.out.println();
     }
 
-    static int[][] getPitch() {
-        return new int[][]{
-                {00, 00, 00, 00, 00, 00, 00},
-                {00, 00, 00, 00, 00, 00, 00},
-                {00, 00, 00, 00, 00, 00, 00},
-                {00, 00, 00, 00, 00, 00, 00},
-                {00, 00, 00, 00, 00, 00, 00},
-                {00, 00, 00, 00, 00, 00, 00},
-                {00, 00, 00, 00, 00, 00, 00}
+    static String[][] getPitch() {
+        return new String[][]{
+                {"", "", "", "", "", "", ""},
+                {"", "", "", "", "", "", ""},
+                {"", "-1", "-1", "-1", "-1", "", ""},
+                {"", "-1", "", "0", "", "-1", ""},
+                {"", "", "", "", "", "-1", ""},
+                {"", "", "", "", "", "", ""},
+                {"", "", "", "", "", "", ""}
         };
     }
 
-
-    static void fillingFreePitches(int[][] pitch, int ax, int ay, int counter) {
-        fillingDownPitch(pitch, ax, ay, counter);
-        fillingUpPitch(pitch, ax, ay, counter);
-        fillingLeftPitch(pitch, ax, ay, counter);
-        fillingRightPitch(pitch, ax, ay, counter);
-        pitch[ay][ax] = 0;
-    }
-
-    static void fillingRightPitch(int[][] pitch, int ax, int ay, int counter) {
-        int tempAX = ax + 1;
-        if (pitch[ay][tempAX] != counter + 1 && pitch[ay][tempAX] != -1) {
-            pitch[ay][tempAX] = counter + 1;
-            if (tempAX < pitch[ay].length - 1) {
-                fillingRightPitch(pitch, tempAX, ay, pitch[ay][tempAX]);
-            }
+    static void startFillingPitches(String[][] pitch, ArrayList list, Integer mark){
+        while(list.size() > 0){
+            fillingPitches(pitch, list, mark);
+            getNewCoord(pitch, list);
+            mark += 1;
         }
     }
+    static void firstStep(String[][] pitch, int ax, int ay, ArrayList list) {
+        stepRight(pitch, ax, ay, list);
+        stepLeft(pitch, ax, ay, list);
+        stepUp(pitch, ax, ay, list);
+        stepDown(pitch, ax, ay , list);
+    }
 
-    static void fillingLeftPitch(int[][] pitch, int ax, int ay, int counter) {
-        int tempAX = ax - 1;
-        if (pitch[ay][tempAX] != counter + 1 && pitch[ay][tempAX] != -1) {
-            pitch[ay][tempAX] = counter + 1;
-            if (tempAX > 0) {
-                fillingLeftPitch(pitch, tempAX, ay, pitch[ay][tempAX]);
-            }
+    static void stepUp(String[][] pitch, int ax, int ay, ArrayList list) {
+        if (ay > 0 && pitch[ay - 1][ax].isEmpty()) {
+            list.add(new int[] {ay-1,ax});
         }
     }
 
-    static void fillingUpPitch(int[][] pitch, int ax, int ay, int counter) {
-        int tempAY = ay - 1;
-        if (pitch[tempAY][ax] != counter + 1 && pitch[tempAY][ax] != -1) {
-            pitch[tempAY][ax] = counter + 1;
-            if (tempAY - 1 >= 0) {
-                fillingUpPitch(pitch, ax, tempAY, pitch[tempAY][ax]);
-            }
-        }
-        if (ax + 1 < pitch[ay].length - 1) {
-            fillingRightPitch(pitch, ax, tempAY, pitch[tempAY][ax]);
-        }
-        if (ax - 1 >= 0) {
-            fillingLeftPitch(pitch, ax, tempAY, pitch[tempAY][ax]);
+    static void stepRight(String[][] pitch, int ax, int ay, ArrayList list) {
+        if (ax < pitch[ay].length-1 && pitch[ay][ax+1].isEmpty()) {
+            list.add(new int[] {ay,ax+1});
         }
     }
 
-    static void fillingDownPitch(int[][] pitch, int ax, int ay, int counter) {
-        int tempAY = ay + 1;
-        if (pitch[tempAY][ax] == 0 && pitch[tempAY][ax] != -1) {
-            pitch[tempAY][ax] = counter + 1;
-            if (tempAY < pitch.length - 1) {
-                fillingDownPitch(pitch, ax, tempAY, pitch[tempAY][ax]);
-            }
+    static void stepLeft(String[][] pitch, int ax, int ay, ArrayList list) {
+        if (ax - 1 >= 0 && pitch[ay][ax - 1].isEmpty()) {
+            list.add(new int[] {ay,ax-1});
         }
-        if (ax + 1 < pitch[ay].length - 1) {
-            fillingRightPitch(pitch, ax, tempAY, pitch[tempAY][ax]);
+    }
+
+    static void stepDown(String[][] pitch, int ax, int ay, ArrayList list) {
+        if (ay+1 < pitch.length && pitch[ay + 1][ax].isEmpty()) {
+            list.add(new int[] {ay+1,ax});
         }
-        if (ax - 1 >= 0) {
-            fillingLeftPitch(pitch, ax, tempAY, pitch[tempAY][ax]);
+    }
+
+    static void fillingPitches(String[][] pitch, ArrayList list, Integer mark){
+        for (int i = 0; i < list.size(); i++) {
+            int[] ar = (int[]) list.get(i);
+            pitch[ar[0]][ar[1]] = mark.toString();
         }
+    }
+
+    static void getNewCoord(String[][] pitch, ArrayList list){
+        ArrayList<int[]>copy = copyOfList(list);
+        list.clear();
+        for (int i = 0; i < copy.size(); i++) {
+            int[] ar = copy.get(i);
+            stepDown(pitch, ar[1], ar[0], list);
+            stepLeft(pitch, ar[1], ar[0], list);
+            stepRight(pitch, ar[1], ar[0], list);
+            stepUp(pitch, ar[1], ar[0], list);
+        }
+    }
+    static ArrayList copyOfList(ArrayList list){
+        ArrayList<int[]> copy = new ArrayList<>();
+        for (Object item : list ) {
+            copy.add((int[]) item);
+        }
+        return copy;
     }
 }
